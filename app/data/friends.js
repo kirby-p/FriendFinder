75ce312e17f829e44a8f711540ca6ae0ac0989a5
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-07-02 09:11:30 -0400

    Added sample questions, able to take value of each answer

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friendsArray = [
+	{
+		"name": "Tim Bouregard",
+		"photo": "https://s3.amazonaws.com/crowdcontent/crowdblog/Man-Smiling-with-Beard.jpg",
+		"scores": [
+			4,
+			2,
+			2
+		]
+	},
+	{
+		"name": "Shelley Mables",
+		"photo": "http://calcagnodds.com/wp-content/uploads/2013/12/pretty-young-woman-with-a-gorgeous-smile.jpg",
+		"scores": [
+			5,
+			3,
+			4
+		]
+	}	
+];
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express"),
 	path = require("path"),
 	bodyParser = require("body-parser"),
 	app = express(),
-	port = 4000;
+	Port = 4000;
 
 // Sets up the Express app to handle data parsing 
 app.use(bodyParser.json());
@@ -13,6 +13,6 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 require('./app/routing/api-routes.js')(app); 
 require('./app/routing/html-routes.js')(app);
 
-app.listen(PORT, function() {
-	console.log("App listening on PORT: " + PORT);
+app.listen(Port, function() {
+	console.log("App listening on PORT: " + Port);
 });
